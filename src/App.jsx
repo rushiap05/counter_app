03f65fc34986252cb4f8c80da53753d94e466d27
rushiap05 <rushiapohare@gmail.com>
2022-04-26 14:48:58 +0530

    input taking is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
-import { useEffect } from 'react';
-import { useState } from 'react'
+
 import './App.css'
 import Counter from './components/Counter'
 
 function App() {
 
-  // const [newNum, setNewNum] = useState(0);
-  // const [sendNum , setSendNum] = useState(1);
-
-  // const takeInput = (event) => {
-  //   console.log(event.target.value)
-  //   if(newNum < 1000){
-  //   setNewNum(event.target.value)
-  //   }
-  //   else {
-  //     alert("Not bigger than 1000")
-  //   }
-  // }
-
-  // useEffect(()=> {
-
-  // },[sendNum])
-
 
   return (
     <div className="App">
-        {/* <div>
-          <input onChange={takeInput} type="number" />
-          <button onClick={() => {
-            console.log("clicked input")
-            setSendNum(newNum)
-          }}>Take Input</button>
-        </div> */}
         <Counter />
     </div>
   )
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -15,9 +15,24 @@ const Counter = () => {
                     console.log(event.target.value)
                     setTakeNum(event.target.value)
                 }} type="number" />
+
+                {/*
+                    Here onChange evenlistener is used to take the input and store it in a variable
+                */}
+
+
                 <button onClick={() => {
-                    setNum(takeNum)
+                    if (Number(takeNum) < 1000) {
+                        setNum(Number(takeNum))
+                    }
+                    else {
+                        alert("Not more than 1000")
+                    }
                 }}>Click</button>
+
+                {/*
+                    button will take input from input section and will place it in the value of variable which is used in counter
+                */}
             </div>
             <div className='counter_div'>
                 <button className='decrease_button'
@@ -26,7 +41,9 @@ const Counter = () => {
                             setNum(num - 1)
                     }}
                 >-</button>
+
                 <div className='counter_show'>{num}</div>
+
                 <button className='increase_button'
                     onClick={() => {
                         if (num < 1000)
